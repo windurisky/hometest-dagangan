40bbd5c4c877c1aa582e862fb004c566806633c0
Windu Risky Adiatma <30619149+windurisky@users.noreply.github.com>
2023-09-13 10:11:26 +0000

    Add gomod and draft the basic domain model

---
+++ domain/fare_configuration.go
@@ -0,0 +1,10 @@
+package domain
+
+type FareConfiguration struct {
+	UpperLimit     int64 `json:"upper_limit"`
+	FarePerMileage int64 `json:"fare_per_mileage"`
+}
+
+type FareConfigurationUsecase interface {
+	GetList() ([]FareConfiguration, error)
+}
